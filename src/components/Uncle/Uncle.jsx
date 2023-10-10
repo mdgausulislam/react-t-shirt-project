1aaa39772a93ea32755941276ef72bc6e5e1e642
unknown <gausulislam5@gmail.com>
2023-10-10 17:49:49 +0600

    update section

--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../GrandPa/GrandPa';
 
 const Uncle = () => {
+    const [money,setMoney]=useContext(MoneyContext);
     return (
         <div>
             <h1>Uncle</h1>
+            <p><small>grandpa money:{money}</small></p>
+            <button onClick={()=>setMoney(money+1000)}>Send Money 1000</button>
             <section className='flex'>
                 <Cousin>Nabil</Cousin>
                 <Cousin>Nabila</Cousin>
